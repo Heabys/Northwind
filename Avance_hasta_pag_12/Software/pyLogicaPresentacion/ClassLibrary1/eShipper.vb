36b6b0e74b670be48a00e30f9e4317c23a628819
Daviz777 <death.kaos@hotmail.com>
2018-01-02 20:29:10 -0500

    avance pagina 12

---
+++ Avance_hasta_pag_12/Software/pyLogicaPresentacion/ClassLibrary1/eShipper.vb
@@ -0,0 +1,8 @@
+﻿Public Class eShipper
+
+    Public Property ShipperId As Integer
+
+    Public Property CompanyName As String
+
+    Public Property Phone As String
+End Class
---
+++ Avance_hasta_pag_12/Software/pyLogicaPresentacion/pyLogicaPresentacion/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("pyLogicaPresentacion")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("pyLogicaPresentacion")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("4c1ff143-ff8c-40d8-8bb3-f22c79611c28")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+' mediante el carácter '*', como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
