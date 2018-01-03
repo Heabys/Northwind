7aeef9fad537f6e41c747f1a97987a649224d887
Ruben <g.carlo97@hotmail.com>
2018-01-02 20:45:33 -0500

    Modulo General

---
+++ Avance_hasta_pag_12/Software/pyLogicaPresentacion/pyLogicaDatos/ModuloGeneral.vb
@@ -0,0 +1,11 @@
+﻿Module ModuloGeneral
+
+    Public Function CadenaConexionDB() As String
+        Dim rtn As String
+
+        rtn = "Data Source=(local); initial Catalog=Northwind; User ID=sa; Password=$sql2014”
+
+        Return rtn
+    End Function
+End Module
+
